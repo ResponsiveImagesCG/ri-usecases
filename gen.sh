b190551b180cc552b659f1ff38f6dadeed4ff3c9
Marcos Caceres <marcosscaceres@gmail.com>
2012-10-28 11:35:54 +0000

    small cleanup of markup, added support for terminal-notifier in spec generator script

--- gen.sh
+++ gen.sh
@@ -1,2 +1,5 @@
 anolis --omit-optional-tags --quote-attr-values --w3c-compat-xref-a-placement --filter=".publish, .w3conly, title + style" index.src.html index.html
-open index.html
+#dependency: sudo gem install terminal-notifier
+if command -v terminal-notifier >/dev/null 2>&1; then
+    terminal-notifier -message "Finished processing ${PWD##*/}" -title "Anolis Processing done" -execute "open $PWD/index.html" -group "Anolis"
+fi
