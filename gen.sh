be90ee8d295fdc618c6382cd1bbea5140a1010e4
Marcos Caceres <marcosscaceres@gmail.com>
2012-10-28 18:39:48 +0000

    Fixed table of contents. Added HTML5 tidy support in generator.

--- gen.sh
+++ gen.sh
@@ -1,5 +1,13 @@
+#!/bin/bash
 anolis --omit-optional-tags --quote-attr-values --w3c-compat-xref-a-placement --filter=".publish, .w3conly, title + style" index.src.html index.html
+
+#dependency: html5 tidy https://github.com/w3c/tidy-html5
+if command -v tidy >/dev/null 2>&1; then
+	tidy -q -utf8 -i -o index.html index.html
+fi
+
 #dependency: sudo gem install terminal-notifier
 if command -v terminal-notifier >/dev/null 2>&1; then
     terminal-notifier -message "Finished processing ${PWD##*/}" -title "Anolis Processing done" -execute "open $PWD/index.html" -group "Anolis"
 fi
+
