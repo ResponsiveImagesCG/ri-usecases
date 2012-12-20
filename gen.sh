c4acb31185ec9b72e2e300800b437f4f35452a75
Marcos Caceres <marcos.caceres@wacapps.net>
2012-12-20 00:16:51 +0000

    Added seperations between different drafts

--- gen.sh
+++ gen.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-anolis --omit-optional-tags --quote-attr-values --w3c-compat-xref-a-placement --filter=".publish, .w3conly, title + style" index.src.html index.html
+anolis --w3c-shortname="respimg-usecases" --pubdate="15 Jan 2013" --omit-optional-tags --quote-attr-values --w3c-compat-xref-a-placement --filter=".ricg" index.src.html index.html
 
 #dependency: html5 tidy https://github.com/w3c/tidy-html5
 if command -v tidy >/dev/null 2>&1; then
@@ -10,4 +10,3 @@ fi
 if command -v terminal-notifier >/dev/null 2>&1; then
     terminal-notifier -message "Finished processing ${PWD##*/}" -title "Anolis Processing done" -execute "open $PWD/index.html" -group "Anolis"
 fi
-
